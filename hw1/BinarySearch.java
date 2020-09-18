16fa6e109966ab54aca329c978205b6377c31205
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-09-17 22:08:59 -0700

    binary search start

---
+++ hw1/BinarySearch.java
@@ -0,0 +1,13 @@
+class BinarySearch{
+    //binary search on an array of integers
+    static void binarySearch(int[] arr){
+        int length = arr.length;
+        for(int i=0; i<length; i++){
+            
+        }
+    }
+    //main method
+    public static void main(String[] args) {
+        BinarySearch.binarySearch(new int[]{1,3,77,88,90,105,110,121,125,159});
+    }
+}
