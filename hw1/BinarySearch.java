ed4aba9efdacd0e82745358aa84db29badf29fc3
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-09-19 12:41:26 -0700

    Binary Search works

--- hw1/BinarySearch.java
+++ hw1/BinarySearch.java
@@ -1,13 +1,37 @@
 class BinarySearch{
     //binary search on an array of integers
-    static void binarySearch(int[] arr){
-        int length = arr.length;
-        for(int i=0; i<length; i++){
-            
+    static void binarySearch(int[] arr, int num){
+        int len = arr.length;
+        int currIndex = len/2;
+        int low = 0;
+        int high = len-1;
+        int mid;
+        while(low <= high){
+            mid = low + (high - low)/2;
+            //check if index mid contains num
+            if(arr[mid]==num){
+                System.out.println(mid + " contains " + num);
+                return;
+            }
+            if(arr[mid] < num){
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
         }
+        System.out.println("Your searched number is not found");
     }
     //main method
     public static void main(String[] args) {
-        BinarySearch.binarySearch(new int[]{1,3,77,88,90,105,110,121,125,159});
+        int[] arr = new int[]{1,3,77,88,90,105,110,121,125,159};
+        //test last index
+        BinarySearch.binarySearch(arr,159);
+        //test first index
+        BinarySearch.binarySearch(arr,1);
+        //test middle value + 1
+        BinarySearch.binarySearch(arr,110);
+        //test something else
+        BinarySearch.binarySearch(arr,77);
     }
 }
