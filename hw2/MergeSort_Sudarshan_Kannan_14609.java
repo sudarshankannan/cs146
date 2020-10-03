94a60d9a4e3d38acb447ed9b7eb4aa1d9d710e03
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-10-02 21:27:21 -0700

    done with hw2

--- hw2/MergeSort_Sudarshan_Kannan_14609.java
+++ hw2/MergeSort_Sudarshan_Kannan_14609.java
@@ -1,8 +1,51 @@
 import java.util.Scanner;
 import java.io.*;
 class MergeSort_Sudarshan_Kannan_14609{
-    void mergeSort(int[] args){
-
+    void mergeSort(int arr[], int left, int right){
+        if(left < right){
+            int middle = (left + right)/2;
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle+1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+    void merge(int[] arr, int left, int middle, int right){
+        int size1 = middle - left + 1;
+        int size2 = right - middle;
+        int leftArr[] = new int[size1];
+        int rightArr[] = new int[size2];
+        //copy data to arrays
+        for(int i=0; i<size1; i++){
+            leftArr[i] = arr[left+i];
+        }
+        for(int j=0; j<size2; j++){
+            rightArr[j] = arr[middle+1+j];
+        }
+        //merge arrays
+        int i=0;
+        int j=0;
+        int k = left;
+        while(i<size1 && j<size2){
+            if(leftArr[i] <= rightArr[j]){
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<size1){
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while(j<size2){
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
     }
     static int[] readFile(String filename){
         //this is a function to read in an array from a text file
@@ -41,7 +84,5 @@ class MergeSort_Sudarshan_Kannan_14609{
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = MergeSort_Sudarshan_Kannan_14609.readFile("MyList.txt");
-        MergeSort_Sudarshan_Kannan_14609.printArray(arr);
     }
 }
--- hw2/QuickSort_Sudarshan_Kannan_14609.java
+++ hw2/QuickSort_Sudarshan_Kannan_14609.java
@@ -1,6 +1,64 @@
+import java.util.Scanner;
+import java.io.*;
 class QuickSort_Sudarshan_Kannan_1409{
-    void quickSort(int[] arr){
-
+    void quickSort(int[] arr, int low, int high){
+        if(low<high){
+            int partitionIndex = doPartition(arr,low,high);
+            quickSort(arr,low,high);
+            quickSort(arr,partitionIndex+1,high);
+        }
+    }
+    int doPartition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = (low-1);
+        for(int j=low; j<high; j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+    static int[] readFile(String filename){
+        //this is a function to read in an array from a text file
+        File myFile = null;
+        Scanner scan = null;
+        int[] arr = null;
+        try{
+            myFile = new File(filename);
+            scan = new Scanner(myFile);
+            int i = 0;
+            while(scan.hasNextInt()){
+                scan.nextLine();
+                i++;
+            }
+            myFile = new File(filename);
+            scan = new Scanner(myFile);
+            int z = 0;
+            arr = new int[i];
+            while(scan.hasNextInt()){
+                arr[z] = Integer.parseInt(scan.nextLine());
+                z++;
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.toString());
+        }
+        finally{
+            scan.close();
+        }
+        return arr;
+    }
+    static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         
