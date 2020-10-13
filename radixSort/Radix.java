8f71c64b4451fef09a111b61db22050bcc02d7e5
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-10-12 23:13:53 -0700

    radix work

--- radixSort/Radix.java
+++ radixSort/Radix.java
@@ -4,13 +4,58 @@ import java.io.*;
 
 class Radix{
     static void radix(int[] arr){
-        ArrayList<Integer> radixArr[] = null;
+        ArrayList<Integer>[] radixArr = (ArrayList<Integer>[])new ArrayList[10];
         for(int i=0; i<10; i++){
             radixArr[i] = new ArrayList<Integer>();
         }
+        //find maxDigits
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        int maxDigits = 0;
+        while(max>0){
+            max/=10;
+            maxDigits++;
+        }
+        System.out.println("maxDigits: " + maxDigits);
+        //perform sorting
+        int exponent = 1;
+        int divisor = 1;
+        int z = 1;
+        divisor = 1;
+        while(exponent < maxDigits){
+            for(int i=0; i<arr.length; i++){
+                int rIndex = (arr[i]/divisor)%10;
+                System.out.println(rIndex + " " + arr[i]);
+                radixArr[rIndex].add(arr[i]);
+            }
+            int arrayIndex = 0;
+            for(int x=0; x<10; x++){
+                int curr = 0;
+                while(curr < radixArr[x].size()){
+                    if(radixArr[x].size()>0){
+                        arr[arrayIndex] = radixArr[x].get(curr);
+                        //System.out.println("arr[arrayIndex] = " + arr[arrayIndex] + ", expected = " + radixArr[x].get(curr));
+                        arrayIndex++;
+                    }
+                    curr++;
+                }
+            }
+            for(int y=0; y<10; y++){
+                radixArr[y] = null;
+                radixArr[y] = new ArrayList<Integer>();
+            }
+            Radix.printArray(arr);
+            divisor*=10;
+            exponent++;
+        }
     }
     public static void main(String[] args) {
-        int arr[] = Radix.readFile("MyList.txt");
+        //int arr[] = Radix.readFile("MyList.txt");
+        int arr[] = new int[]{170,45,75,90,802,24,2,66};
         Radix.radix(arr);
         Radix.printArray(arr);
     }
