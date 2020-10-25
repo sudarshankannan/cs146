d9fee00f3e74a45a774e89fbb2fe6db0b14027e3
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-10-24 18:01:35 -0700

    tree

--- redblack/Node.java
+++ redblack/Node.java
@@ -2,12 +2,21 @@ class Node{
     int data;
     Node left;
     Node right;
+    Node parent;
+    // 0 - red, 1 - black
+    int color;
     //constructor
     Node(){
         this.left = null;
         this.right = null;
+        this.parent = null;
+        this.color = 0;
     }
     Node(int data){
         this.data = data;
+        this.color = 0;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
     }
 }
---
+++ redblack/Tree.java
@@ -0,0 +1,86 @@
+class Tree{
+    Node root;
+    //constructors
+    Tree(){
+        this.root = null;
+    }
+    //insert
+    void insert(int data){
+        Node nuNode = new Node(data);
+        if(this.root = null){
+            this.root = nuNode;
+            this.color = 1;
+        }
+        else{
+            Node p = this.root;
+            while(p != null){
+                if(data < p.data){
+                    if(p.left == null){
+                        p.left = nuNode;
+                        nuNode.parent = p;
+                        //RECOLOR LOGIC
+                        if(nuNode.parent.color == 0){
+                            recolor(nuNode);
+                        }
+                        return;
+                    }
+                    else{
+                        p = p.left;
+                    }
+                }
+                else if(data > p.data){
+                    if(p.right == null){
+                        p.right = nuNode;
+                        nuNode.parent = p;
+                        //RECOLOR LOGIC
+                        if(nuNode.parent.color == 0){
+                            recolor(nuNode);
+                        }
+                        return;
+                    }
+                    else{
+                        p = p.right;
+                    }
+                }
+            }
+        }
+    }
+    void recolor(Node p){
+        //red parent & red uncle case
+        boolean redred = false;
+        while(p!=this.root){
+            redred = false;
+            if(p.parent.parent != null){
+                if(p.parent.parent.right.color == 0 && p.parent.parent.right != p.parent){
+                    p.parent.parent.right.color = 1
+                    p.parent = 1;
+                    redred = true;
+                }
+                else if(p.parent.parent.left.color == 0 && p.parent.parent.left != p.parent){
+                    p.parent.parent.left.color = 1
+                    p.parent = 1;
+                    redred = true;
+                }
+            }
+            //change p=p's grandparent
+            if(redred == true){
+                p = p.parent.parent;
+            }
+            else{
+                break;
+            }
+        }
+        //red parent and black uncle case
+        while(p!=this.root){
+            redred = false;
+            if(p.parent.parent != null){
+                if(p.parent.parent.right.color == 0 && p.parent.parent.right != p.parent){
+                    //insert
+                }
+                else if(p.parent.parent.left.color == 0 && p.parent.parent.left != p.parent){
+                    //insert
+                }
+            }
+        }
+    }
+}
