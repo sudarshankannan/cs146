2e053810f98b083dd8dafce02e128e15cc99deaf
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-10-24 17:03:21 -0700

    first

---
+++ redblack/Node.java
@@ -0,0 +1,13 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    //constructor
+    Node(){
+        this.left = null;
+        this.right = null;
+    }
+    Node(int data){
+        this.data = data;
+    }
+}
